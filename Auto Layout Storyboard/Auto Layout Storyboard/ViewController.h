b768c1437c581a0e29b2c1426ddd5b5d2b8c1141
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-05 17:43:45 -0700

    Done

---
+++ Auto Layout Storyboard/Auto Layout Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Auto Layout Storyboard
+//
+//  Created by Martin Zhang on 2016-07-05.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
